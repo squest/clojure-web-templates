de01585d5c27bb605bfd6390ca6fe5de3d4f02bb
Sabda PS <sabdaps@gmail.com>
2024-04-08 17:10:25 +0700

    simplified cljs

--- simplified-fullstack/app/src/cljs/app/content/events.cljs
+++
@@ -1,1 +0,0 @@
-(ns app.content.events)
--- simplified-fullstack/app/src/cljs/app/db.cljs
+++ simplified-fullstack/app/src/cljs/app/db.cljs
@@ -1,5 +1,40 @@
 (ns app.db)
 
+(defonce skill-groups
+         [{:name     "Fundamental skills"
+           :id       (str (random-uuid))
+           :children [{:name     "Thinking skills"
+                       :id       (str (random-uuid))
+                       :children [{:name     "Creative thinking"
+                                   :id       (str (random-uuid))
+                                   :children []}
+                                  {:name     "Critical thinking"
+                                   :id       (str (random-uuid))
+                                   :children [{:name     "Math skills"
+                                               :id       (str (random-uuid))
+                                               :children []}]}
+                                  {:name     "Problem solving"
+                                   :id       (str (random-uuid))
+                                   :children []}]}]}
+          {:name     "Practical skills"
+           :id       (str (random-uuid))
+           :children [{:name     "Business and marketing"
+                       :id       (str (random-uuid))
+                       :children [{:name     "marketing"
+                                   :id       (str (random-uuid))
+                                   :children []}]}
+                      {:name     "Technology"
+                       :id       (str (random-uuid))
+                       :children [{:name     "Computer programming"
+                                   :id       (str (random-uuid))
+                                   :children []}]}]}
+          {:name     "Academic skills"
+           :id       (str (random-uuid))
+           :children [{:name     "Mathematics and statistics"
+                       :id       (str (random-uuid))
+                       :children []}]}])
+
 (def default-db
-  {:name         "Clojure fullstack app"
+  {:name         "Template"
+   :skill-groups skill-groups
    :base-url     "http://localhost:8000"})
